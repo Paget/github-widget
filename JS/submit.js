f0d4d4dd85d6e057cd53d3f3a8e1a8b21c987ccf
Paget Fink <pagetfink@gmail.com>
2015-02-07 21:38:33 -0500

    api working and basic styling

--- JS/submit.js
+++ JS/submit.js
@@ -1,21 +1,35 @@
 $(function() {
   var userData = $('#user-data').html();
-  var templete = _.template(userData, {variable: 'm'});
+  var template = _.template(userData, {variable: 'm'});
+  var repoData = $('#repose').html();
+  var template2 = _.template(repoData, {variable: 'm'});
 
   $('.form').submit(function(){
     var username = $('.username-input').val();
 
     $.getJSON('https://api.github.com/users/' + username)
+
       .done(function (data) {
         console.log(data);
-        $('.wrapper').html(templete(data));
+        $('.wrapper').html(template(data));
+        $('.show-repos').click(function() {
+          console.log(data.repos_url);
+          showRepos(data.repos_url);
+        });
       })
 
       .fail(function(request, status, error) {
         console.log('This failed because' + error);
-
       });
+
     return false;
   });
 
+  function showRepos(url) {
+    console.log('show me the omony', typeof url);
+    $.getJSON(url)
+      .done(function (data){
+        $('.repos-list').html(template2(data));
+      })
+    }
 });
